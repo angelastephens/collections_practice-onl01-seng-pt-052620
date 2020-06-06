78bb20467b87af2f0635ef657e97059de536bb52
Angela Stephens <angelastephens317@gmail.com>
2020-06-06 18:37:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ def kesha_maker(names)
 end
   
 def find_a(words)
-  words.select {|word| word.starts_with?('a')}. first(10)
+  words.select {|word| word.start_with?('a')}. first(10)
   return words
 end
 
