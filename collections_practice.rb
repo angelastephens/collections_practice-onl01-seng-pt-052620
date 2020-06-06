06f4ebb337a3a1e5eb4ee1ca3cb607769341799d
Angela Stephens <angelastephens317@gmail.com>
2020-06-06 19:21:36 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(num)
+ num.sort 
+end
+
+def sort_array_desc(num)
+  num.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(string)
+  string.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(num)
+  num.reverse
+end
+
+def kesha_maker(names)
+  names.each {|name| name[2] = "$"}
+  return names
+end
+  
+def find_a(words)
+  words.select {|word| word.start_with?('a')}
+end
+
+def sum_array(num)
+  num.inject(0){|sum,num| sum + num }
+end
+
+def add_s(words)
+  words.each_with_index.collect do |word,index| 
+  if index == 1 
+    word
+  else
+    word << "s"
+  end
+end
+end
+
 
